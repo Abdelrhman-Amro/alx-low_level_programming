ba0bd86940ae31c0da3ff846c9f2d32ea664d7bb
Abdelrhman <Abdo@gmail.com>
2023-03-18 13:23:20 -0700

    GG

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - here we go
+ *
+ *Description: C program print sting
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
