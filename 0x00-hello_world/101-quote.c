408e96e3e07399d517cb57b3796e18b8d50b1c3a
Abdelrhman <Abdo@gmail.com>
2023-03-18 15:49:16 -0700

    GG Bro

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Start here
+ *
+ * Description: Using fputs
+ *
+ * Return: 1 Always Success
+*/
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
