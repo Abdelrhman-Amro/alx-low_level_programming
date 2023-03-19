710353c5ad3d2f026ff4103590c19eba3c17e95a
Abdelrhman <Abdo@gmail.com>
2023-03-18 21:23:45 -0700

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start here
+ *
+ * Description: alphabet in lowercasse
+ *
+ * Return: 0 if Success
+*/
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+		putchar(i++);
+	putchar('\n');
+	return (0);
+}
