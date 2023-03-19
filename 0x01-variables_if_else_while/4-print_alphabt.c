9778e4250e585740a6fbbbc026593cd29147e149
Abdelrhman <Abdo@gmail.com>
2023-03-18 21:51:33 -0700

    GG

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - start here
+ *
+ * Description: alphabet in lowercasse except q and e
+ *
+ * Return: 0 if Success
+*/
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		putchar(i++);
+	}
+	putchar('\n');
+	return (0);
+}
