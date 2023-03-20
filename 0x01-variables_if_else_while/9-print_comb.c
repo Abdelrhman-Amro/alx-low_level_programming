378cbdbbbf5142135f0ead5fbc8ebc7f2ce971a9
Abdelrhman <Abdo@gmail.com>
2023-03-20 06:52:49 -0700

    GG'

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,19 @@
 
 int main(void)
 {
-	int i = 48;
+	int i = 48, x = 44;
 
-	while (1)
+	while (i < 58)
 	{
 		putchar(i);
 		if (i == 57)
 		{
-			putchar('$');
-			break;
+			x = 36;
 		}
-		putchar(',');
+		putchar(x);
+		if (i == 57)
+			break;
+		putchar(' ');
 		i++;
 	}
 	putchar('\n');
