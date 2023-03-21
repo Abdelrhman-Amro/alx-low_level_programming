16e82ffb131d316d502425f38ed212fcf7fbf575
Abdelrhman <Abdo@gmail.com>
2023-03-21 02:38:12 -0700

    done 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: print "_putchar"
+ *
+ * Return: on success 1
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
