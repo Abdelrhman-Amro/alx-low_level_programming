5b1f4b1760fe812d21f23e7e93a44278b78e08d6
Abdelrhman <Abdo@gmail.com>
2023-03-21 13:56:17 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,24 +4,24 @@
  * print_to_98 - from n to 98
  *
  * @n: number
- *
- * Return: none
 */
 
 void print_to_98(int n)
 {
 	printf("%d", n);
 
-	int x;
+	int Num;
 
-	x = 1;
+	Num = 1;
 
 	if (n > 98)
-		x = -1;
+		Num = -1;
+
 	while (n != 98)
 	{
-	n += x;
-	printf(", %d", n);
+		n += Num;
+		printf(", %d", n);
 	}
+
 	putchar('\n');
 }
