11dfbbf0e3800df0cb919664f393eb5254c03409
Abdelrhman <Abdo@gmail.com>
2023-03-23 05:37:26 -0700

    done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
