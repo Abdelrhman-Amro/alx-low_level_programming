632a6e54b0a2c0621b86870b7d302c93ca512256
Abdelrhman <Abdo@gmail.com>
2023-03-27 14:00:21 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate length of a string
+ *
+ * @s: conatain string address
+ *
+ * Return: size of string
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '/0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
