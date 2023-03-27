00613e2c72ca2a14e3fcc4f1fc1fdaff70dfa469
Abdelrhman <Abdo@gmail.com>
2023-03-27 16:18:15 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - print one and one no
+ *
+ * @str: string
+*/
+
+void puts2(char *str)
+{
+	int cnt;
+
+	cnt = 0;
+	while (*str != '\0')
+	{
+		if (cnt % 2 == 0)
+			_putchar(*str);
+		str++;
+		cnt++;
+	}
+}
