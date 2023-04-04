889e98bcdcf0cefeb88899a4a23a166669ec03df
Abdelrhman <Abdo@gmail.com>
2023-04-04 16:02:57 -0700

    done

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_diagsums - function
+ *@a: pointer
+ *@size: rows and coloumn
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, Sumx, Sumy;
+
+	i = 0, Sumx = 0;
+	for (; i < size * size; i += size + 1)
+		Sumx += *(a + i);
+
+	i = size - 1, Sumy = 0;
+	for (; i < (size * size) - size + 1; i += size - 1)
+		Sumy += *(a + i);
+
+	printf("%d, %d\n", Sumx, Sumy);
+}
