c720d8c9c34ec8e37a42eed84c91e572dc5e4f04
Abdelrhman <Abdo@gmail.com>
2023-04-05 07:52:01 -0700

    done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - function
+ *@s: string
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
