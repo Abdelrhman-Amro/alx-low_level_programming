5ffe60f40ea60f44d91f23e641adc881f12f3908
Abdelrhman <Abdo@gmail.com>
2023-04-06 18:44:18 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,20 @@
 #include "main.h"
 
-int Nn(int n)
+/**
+ *SQR - function
+ *@n: number
+ *@val: value
+ *Return: square root
+*/
+
+int SQR(int n, int val)
 {
-	if (n == 0)
-		return (0);
-	return (1 + Nn(n / 10));
-}
-
-int Ss(int d)
-{
-	if (d == 1)
-		return (1);
-	return (10 * Ss(d - 1));
-}
-
-int Ee(int d)
-{
-	if (d == 0)
-		return (0);
-	return (9 + 10 * Ee(d - 1));
-}
-
-long double SQR(long double n, long double s, long double e)
-{
-	long double m;
-
-	m = (s+e) / 2;
-
-	if (m * m == n)
-		return m;
-	else if (m * m > n)
-		return (SQR(n, s, m));
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (val + 1);
 	else
-		return (SQR(n, m, e));
+		retutn (-1);
 }
 
 /**
@@ -43,19 +25,5 @@ long double SQR(long double n, long double s, long double e)
 
 int _sqrt_recursion(int n)
 {
-	int d, s, e, x;
-
-	d = (1 + Nn(n)) / 2;
-	s = Ss(d);
-	e = Ee(d);
-	x = SQR(n, s, e);
-
-	long double y;
-
-	y = SQR(n, s, e);
-
-	if (y > x)
-		return (-1);
-	else
-		return (x);
+	return (SQR(n, 1);
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int is_prime_number(int n)
+{
+	if (!(n % 2) && !(n%5))
+		return 0;
+}
