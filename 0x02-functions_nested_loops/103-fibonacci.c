a2f0c61b44830f10fc5291c65c576045de8cbd52
Abdelrhman <Abdo@gmail.com>
2023-04-08 12:42:07 -0700

    done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - function
+ *Return: Always 0
+*/
+
+int main(void)
+{
+        unsigned long x, y, z, sum;
+
+        x = 1, y = 2, sum = 0;
+        for (z = x + y; z <= 4000000; z = x + y)
+        {
+                if (z % 2 == 0)
+			sum += z;
+                x = y;
+                y = z;
+        }
+        printf("%lu\n", sum);
+        return (0);
+}
+
