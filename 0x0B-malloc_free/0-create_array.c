3b1ac0e243459a8ee809ddc526ea667e554fb4b8
Abdelrhman <Abdo@gmail.com>
2023-04-11 18:22:13 -0700

    done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *create_array - function
+ *@size: size
+ *@c: char to fill
+ *Return: Null or arr
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr = malloc(1 * size);
+
+	if (arr == NULL || size == 0)
+		return (0);
+
+	while (size--)
+		arr[size] = c;
+
+	return (arr);
+}
