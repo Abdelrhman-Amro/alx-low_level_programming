63bd87be0c897ebb5911f0c9e4b30b42c49c8bb3
Abdelrhman <Abdo@gmail.com>
2023-04-19 23:27:59 -0700

    done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,12 @@ char *_strcpy(char *dest, char *src)
 	char *cp = dest;
 	int i = 0;
 
-	while (src[i] >= '\0')
+	while (src[i] != '\0')
 	{
 		cp[i] = src[i];
 		i++;
 	}
+	cp[i] = src[i];
 
 	return (dest);
 }
