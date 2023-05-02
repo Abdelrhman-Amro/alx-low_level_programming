3470fa0b63063fb1ed44e0d7a62d7ee021e8c1d3
Abdelrhman <Abdo@gmail.com>
2023-05-02 05:23:58 -0700

    done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - function
+ *@head: pointer to pointer
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = *head->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
