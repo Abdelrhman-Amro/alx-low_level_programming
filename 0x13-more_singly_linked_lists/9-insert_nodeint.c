cdc5ddc476144952fa05fe6dc311205b367b0692
Abdelrhman <Abdo@gmail.com>
2023-05-02 18:17:18 -0700

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		Node = Node->next;
 		i++;
 	}
-	if (idx > i)
+	if (idx - 1 > i)
 	{
 		free(New);
 		return (NULL);
