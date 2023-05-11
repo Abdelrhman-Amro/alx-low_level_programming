f7c9d2a7635d994a0db0f12dbfcbdf625d1c9897
Abdelrhman-Amro <abdamro288dev@gmail.com>
2023-05-10 22:23:31 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,24 @@
 #include "main.h"
+
+/**
+ * STI - function
+ * @S: string
+ * Return: I
+*/
+
+int STI(char *S)
+{
+	int I, i;
+
+	i = 0, I = 0;
+	while (S[i] != '\0')
+	{
+		I = I * 10 + (S[i] - '0');
+		i++;
+	}
+	return (I);
+}
+
 /**
  * main - function
  * @argc: number
@@ -10,7 +30,7 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%d\n", (argv[1] * argv[2]));
+		printf("%d\n", (STI(argv[1]) * STI(argv[2])));
 		return (0);
 	}
 	printf("Error\n");
