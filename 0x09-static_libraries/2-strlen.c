73e68f726bbe09d86470882e6cb7b8d8ad50f4a1
Abdelrhman <abdamro288dev@gmail.com>
2023-05-10 20:00:54 -0700

    done

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -6,4 +6,15 @@
  *Return: I
 */
 
-int _strlen(char *s);
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,10 +1,27 @@
 #include "main.h"
-
 /**
- *_strcpy - function
- *@dest: D
- *@src: S
- *Return: C
-*/
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
 
-char *_strcpy(char *dest, char *src);
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
