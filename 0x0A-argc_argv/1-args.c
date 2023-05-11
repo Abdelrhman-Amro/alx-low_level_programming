86435661513607ffd60e2ddac5e1b937df42dbb6
Abdelrhman-Amro <abdamro288dev@gmail.com>
2023-05-10 21:28:30 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: number
+ * @argv: string
+ * Return: 0
+*/
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
