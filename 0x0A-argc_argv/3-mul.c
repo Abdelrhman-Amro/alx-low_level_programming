1e15c306653ad0be9e0d32d627ee4084775e8454
Abdelrhman-Amro <abdamro288dev@gmail.com>
2023-05-10 22:44:45 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * STI - function
+ * @S: string
+ * Return: I
+*/
+
+int STI(char *S)
+{
+	int I = 0, i = 0, minus = 1;
+
+	if (S[i] == '-')
+		i++, minus = -1;
+	while (S[i] != '\0')
+	{
+		I = I * 10 + (S[i++] - '0');
+	}
+	I *= minus;
+	return (I);
+}
+
+/**
  * main - function
  * @argc: number
  * @argv: string
