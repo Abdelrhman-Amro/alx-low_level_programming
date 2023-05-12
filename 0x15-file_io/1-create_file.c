5133dafd30b156e529c70431039b838df37464b0
Abdelrhman-Amro <abdamro288dev@gmail.com>
2023-05-12 07:37:42 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 	{
-		close(fd);
 		return (1);
 	}
 	while (text_content[ln] != '\0')
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function
+ * @filename: name
+ * @text_content: txt
+ * Return: I
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{ 
+	return (1);
+}
