fa99fbaeba22829759ce8ca93165bf9ef5be9259
Abdelrhman-Amro <abdamro288dev@gmail.com>
2023-05-12 02:22:44 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_file - Function
+ * @filename: name
+ * @text_content: txt
+ * Return: number
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int ln = 0;
+	int fd = open(filename, O_CREATE | O_RDONLY, 0600);
+
+	while (text_content[ln] != '\0')
+		ln++;
+	ln = write(fd, text_conten, ln);
+	close(fd);
+	return (1);
+}
