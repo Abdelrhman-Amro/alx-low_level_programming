699b636a8101295e3aed7171bf4e7051d08e9f16
Abdelrhman-Amro <abdamro288dev@gmail.com>
2023-05-12 02:25:48 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int ln = 0;
-	int fd = open(filename, O_CREATE | O_RDONLY, 0600);
+	int fd = open(filename, O_CREAT | O_WRONLY, 0600);
 
 	while (text_content[ln] != '\0')
 		ln++;
