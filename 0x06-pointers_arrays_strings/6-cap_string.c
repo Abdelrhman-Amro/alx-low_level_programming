e2af32b9aa4cccb20418b688ed76bb3dc4a0945e
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-09-15 17:20:55 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,33 @@
 char *cap_string(char *str)
 {
     char *sp = " \t\n,;.!?(){}\"";
-    int i = 0, j;
+    int i , j;
 
-    
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            continue;
+        else
+        {
+            for (j = 0; sp[j] != '\0'; j++)
+            {
+                if (sp[j] == str[i])
+                {
+                    i++;
+                    while (str[i] != '\0' && (str[i] < 'A' || str[i] > 'Z'))
+                    {
+                        if (str[i] >= 'a' && str[i] <= 'z')
+                        {
+                            str[i] -= 32;
+                            break;
+                        }
+                        i++;
+                    }
+                    break;
+                }
+            }
+
+        }
+    }
     return (str);
 }
