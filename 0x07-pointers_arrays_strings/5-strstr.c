7954fefd2b6e8e897b56130b9e24c284437fd77e
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-09-16 12:08:03 +0300

    done 5555555

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,28 +2,29 @@
 
 /**
  *_strstr - function
- *
  *@haystack: Original string
- *
  *@needle: sub string
- *
  *Return: pointer Or NULL
 */
-
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
 	for (i = 0; haystack[i] >= '\0'; i++)
 	{
-		if (haystack[i] == needle[0])
+		j = 0;
+		if (haystack[i] == needle[j])
 		{
 			i++;
-			for (j = 1; haystack[i] == needle[j] && needle[j] != '\0'; j++)
+			j++;
+			while (haystack[i] == needle[j] && needle[j] != '\0')
+			{
 				i++;
+				j++;
+			}
 			if (needle[j] == '\0')
 				return (haystack + i - j);
 		}
 	}
-	return ('\0');
+	return (0);
 }
