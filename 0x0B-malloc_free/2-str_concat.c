7ed86eb2c18edcb5fd7605e1774f024d74dcff59
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-09-18 19:58:10 +0300

    done 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * slen - string length
+ * @s: string
+ * Return: length
+*/
+int slen(char *s)
+{
+	int ln = 0;
+
+	while (*s != '\0')
+	{
+		ln++;
+		s++;
+	}
+	return (ln);
+}
+
+/**
+ * str_concat - string concat
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to new string
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *s3;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	s3 = (char *)malloc(sizeof(char) * (slen(s1) + slen(s2) + 1));
+	if (s3 == NULL)
+		return (NULL);
+	for (i = 0, j = 0; s1[j] != '\0'; i++, j++)
+		s3[i] = s1[j];
+	for (j = 0; s2[j] != '\0'; j++, i++)
+		s3[i] = s2[j];
+	s3[i] = '\0';
+	return (s3);
+}
