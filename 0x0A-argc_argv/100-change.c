0c55f739a1bd684966063f2f64609ef50094fee4
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-09-18 18:20:52 +0300

    now gg

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,28 +8,26 @@
  **/
 int main(int argc, char **argv)
 {
-	int cents[] = {1, 2, 5, 10, 25};
+	int cents[] = {25, 10, 5, 2, 1};
 	int money, cnt = 0, i;
 
 	if (argc != 2)
-		return (puts("Error"), 1);
+	{
+		puts("Error");
+		return (1);
+	}
 	money = atoi(argv[1]);
 	if (money <= 0)
 		puts("0");
 	else
 	{
-		while (money)
-		{
-			for (i = 4; i >= 0; i--)
+			for (i = 0; i < 5; i++)
 			{
-				if (money % cents[i] == 0)
-				{
-					money -= cents[i];
-					cnt++;
+				cnt += money / cents[i];
+				money = money % cents[i];
+				if (money == 0)
 					break;
-				}
 			}
-		}
 		printf("%d\n", cnt);
 	}
 	return (0);
