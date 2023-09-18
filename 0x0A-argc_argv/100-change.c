43e77d3b3a59accc7812e9c03716dc63d3e7dcc0
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-09-18 17:51:57 +0300

    done 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
+/**
+ * main - Program
+ * @argc: c
+ * @argv: vars
+ * Return: 0
+ **/
 int main(int argc, char **argv)
 {
-    int cents[5] = {1, 2, 5, 10, 20};
-    int money, cnt = 0, i;
+	int cents[] = {1, 2, 5, 10, 20};
+	int money, cnt = 0, i;
 
-    if (argc > 2)
-        return (puts("Error"), 1);
-
-    money = atoi(argv[1]);
-
-    if (money <= 0)
-        puts("0");
-    else
-    {
-        while (money)
-        {
-            for (i = 4; i >= 0; i--)
-            {
-                if (money % cents[i] == 0)
-                {
-                    money -= cents[i];
-                    cnt++;
-                    break;
-                }
-            }
-        }
-        printf("%d\n", cnt);
-    }
-    
-    return (0);
+	if (argc != 2)
+		return (puts("Error"), 1);
+	money = atoi(argv[1]);
+	if (money <= 0)
+		puts("0");
+	else
+	{
+		while (money)
+		{
+			for (i = 4; i >= 0; i--)
+			{
+				if (money % cents[i] == 0)
+				{
+					money -= cents[i];
+					cnt++;
+					break;
+				}
+			}
+		}
+		printf("%d\n", cnt);
+	}
+	return (0);
 }
