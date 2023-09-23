943b8ed0773e9f23fe2c451cd138afd00f4c6e73
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-09-23 16:01:53 +0300

    NOw it is fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,8 @@ int str_len(const char *s)
 {
 	int len = 0;
 
+	if (!s)
+		return (0);
 	while (s[len] != '\0')
 		len++;
 	return (len);
--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -10,11 +10,11 @@ int main(void)
 {
     unsigned int n;
 
-    n = binary_to_uint("1");
+    n = binary_to_uint(NULL);
     printf("%u\n", n);
     n = binary_to_uint("101");
     printf("%u\n", n);
-    n = binary_to_uint("1e01");
+    n = binary_to_uint("10000000000000000000");
     printf("%u\n", n);
     n = binary_to_uint("1100010");
     printf("%u\n", n);
