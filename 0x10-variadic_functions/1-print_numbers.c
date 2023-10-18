580b4316a0b19946105ddedbbf73f63f26757f84
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-18 15:29:35 +0300

    TASK 1 COMPLETED

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers with sparator
+ * @separator: separator to print between numbers
+ * @n: number of vars to print
+ * @...: numbers to print
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i, sz = n;
+
+	va_start(args, n);
+	printf("%d", va_arg(args, int));
+	if (separator == NULL)
+	{
+		for (i = 0; i < sz; i++)
+			printf("%d", va_arg(args, int));
+	}
+	else
+	{
+		for (i = 0; i < sz; i++)
+			printf("%s%d", separator, va_arg(args, int));
+	}
+	printf("\n");
+	va_end(args);
+}
