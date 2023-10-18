ef8fbe0e28557e9be58e476bb44ecdf50c12697b
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-18 15:03:21 +0300

    TASK 0 done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum numbers
+ * @n: number of integer to sum
+ * @...: Varibales to sum
+ * Return: sum of numbers
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i, sum;
+
+	va_start(args, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
