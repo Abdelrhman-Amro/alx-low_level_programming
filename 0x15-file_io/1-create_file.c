473de6ea27f507ce046486a69912b14bbc6cb540
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-20 00:09:02 +0300

    fix betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,20 +26,20 @@ int _strlen(char *s)
 */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, nby, len;
+	int fd, nby, len;
 
-    if (!filename)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
-    if (text_content)
-    {
-        len = _strlen(text_content);
-        nby = write(fd, text_content, len);
-        if (nby == -1)
-            return (-1);
-    }
-    close(fd);
-    return (1);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		len = _strlen(text_content);
+		nby = write(fd, text_content, len);
+		if (nby == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
