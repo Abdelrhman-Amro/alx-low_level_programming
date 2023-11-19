1240567e9a1c1fafe9f02998fa49c7c08ed1abef
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 15:56:10 +0200

    OK task  4 completed

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,4 +4,14 @@
  * free_dlistint - free the dlist
  * @head: head of the dlist
 */
-void free_dlistint(dlistint_t *head);
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *to_free = head;
+
+	while (head)
+	{
+		to_free = head;
+		head = head->next;
+		free(to_free);
+	}
+}
