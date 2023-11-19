2b2cda3c06d6e237bb5c00c42d78568e7f5c7d7b
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 20:31:11 +0200

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,4 +7,37 @@
  * @n: value of the new node
  * Return: address of the new node, or NULL if it failed
 */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node;
+	dlistint_t *node_idx;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node ==  NULL)
+		return (NULL);
+	/***************check if add at the beginning********************/
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	/****************find node idx*******************/
+	node_idx = *h;
+	while (node_idx && idx)
+	{
+		node_idx = node_idx->next;
+		idx--;
+	}
+	if (idx != 0)
+		return (NULL);
+
+	/*********************check if add at the end***********************/
+	if (node_idx->next == NULL)
+		return (add_dnodeint_end(&node_idx, n));
+
+	/********************add node in the middle***********************/
+	node->n = n;
+	node->next = node_idx;
+	node->prev = node_idx->prev;
+	node_idx->prev->next = node;
+	node_idx->prev = node;
+	return (node);
+}
