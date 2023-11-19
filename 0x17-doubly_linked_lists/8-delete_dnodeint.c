a8c173e22f8ec71b73824944b355352f269eb7b1
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 22:00:35 +0200

    task 8 completed with very efficient solution

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -6,4 +6,25 @@
  * @index: index to search for
  * Return: 1 if it succeeded, -1 if it failed
 */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node = *head;
+	unsigned int select = 0;
+
+	while (node && select < index)
+	{
+		node = node->next;
+		select++;
+	}
+	if (node == NULL || select < index)
+		return (-1);
+
+	if (index != 0)
+		node->prev->next = node->next;
+	if (index == 0 && node->next != NULL)
+		node->next->prev = node->prev;
+	if (index == 0)
+		*head = node->next;
+	free(node);
+	return (1);
+}
