b39f7111ae139ad8617ca8543aa65455c4209eb4
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 13:27:05 +0200

    task 1 completed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,4 +5,14 @@
  * @h: head of the dlist
  * Return: number of nodes
 */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t cnt = 0;
+
+	while (h)
+	{
+		cnt++;
+		h = h->next;
+	}
+	return (cnt);
+}
