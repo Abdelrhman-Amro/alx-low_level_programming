bae7abe3d91836520b5ab55f893a01782f5fc32b
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 16:29:03 +0200

    task 6 completed

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,4 +5,14 @@
  * @head: head of the dlist
  * Return: Summation of data or NULL if empty
 */
-int sum_dlistint(dlistint_t *head);
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
