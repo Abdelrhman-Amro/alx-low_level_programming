fa5db4e8a11b2b9e63ec8280d875a3b33e8ab06c
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 15:46:48 +0200

    task 3 completed

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,4 +6,23 @@
  * @n: value of the element
  * Return: address of the new element, or NULL if it failed
 */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+	dlistint_t *last;
+
+	last = *head;
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	while (last && last->next)
+		last = last->next;
+	node->prev = last;
+	node->next = NULL;
+	if (!last)
+		*head = node;
+	else
+		last->next = node;
+	return (node);
+}
