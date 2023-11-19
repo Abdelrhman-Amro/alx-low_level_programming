c790c00aaf1e6d76abd2ce789af1bc9e93a685bb
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-19 14:25:04 +0200

    task 2 completed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,4 +6,18 @@
  * @n: value of the new node
  * Return: address of the new element or NULL if failed
 */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *h = *head;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->next = h;
+	node->prev = NULL;
+	if (h != NULL)
+		h->prev = node;
+	*head = node;
+	return (node);
+}
