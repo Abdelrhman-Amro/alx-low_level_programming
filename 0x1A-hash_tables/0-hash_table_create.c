a03f93351a8c32620669b5494d6b73bec2b51f93
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-30 16:18:26 +0200

    Handeled files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the array
+ * Return:	-pointer to the newly created hash table
+ *			-If something went wrong return NULL
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-
-    ht = malloc(sizeof(ht) * (2 + size));
-
-    return (ht);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: key, can not be an empty string
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+}
