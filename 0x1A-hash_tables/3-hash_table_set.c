ea32c8e9225ddbf3af48143f754834f2c38f9844
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-01 00:22:26 +0200

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
 
 /**
+ * creat_item - creat new item
+ * @key: key
+ * @value: value
+ * Return: pointer to new item
+*/
+hash_node_t *creat_item(const char *key, const char *value)
+{
+	hash_node_t *item;
+
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (0);
+	item->key = strdup(key);
+	item->value = strdup(value);
+	if (!item->key || !item->value)
+		return (0);
+	return (item);
+}
+
+/**
  * hash_table_set - add an element to the hash table.
  * @ht: the hash table you want to add or update the key/value to
  * @key: key, can not be an empty string
@@ -9,29 +29,37 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i;
-	hash_node_t *item;
+	unsigned long int idx, update = 0;
+	hash_node_t *item, *node;
 
-	/*Creat Item*/
-	item = malloc(sizeof(hash_node_t));
-	if (item == NULL)
+	if (ht == NULL)
 		return (0);
-	item->key = strdup(key);
-	item->value = strdup(value);
-	if (!item->key || !item->value)
-		return (0);
-
-	/*Find index*/
-	i = key_index((const unsigned char *)key, ht->size);
-
-	/*Check index*/
-	if (ht->array[i] == NULL)
-		ht->array[i] = item;
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[idx])
+	{
+		node = ht->array[idx];
+		while (node)
+		{
+			if (strcmp(node->key, key) == 0)
+			{
+				update = 1;
+				break;
+			}
+			node = node->next;
+		}
+		if (update)
+			ht->array[idx]->value = strdup(value);
+		else
+		{
+			item = creat_item(key, value);
+			item->next = ht->array[idx];
+			ht->array[idx] = item;
+		}
+	}
 	else
 	{
-		item->next = ht->array[i];
-		ht->array[i] = item;
+		item = creat_item(key, value);
+		ht->array[idx] = item;
 	}
-
 	return (1);
 }
