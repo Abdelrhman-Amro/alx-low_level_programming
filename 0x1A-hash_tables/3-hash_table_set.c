8f7e9a661ff0f34bb4764a8a481b18a492f40087
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-30 20:57:39 +0200

    task 3 completed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,4 +9,29 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int i;
+	hash_node_t *item;
+
+	/*Creat Item*/
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (0);
+	item->key = strdup(key);
+	item->value = strdup(value);
+	if (!item->key || !item->value)
+		return (0);
+
+	/*Find index*/
+	i = key_index((const unsigned char *)key, ht->size);
+
+	/*Check index*/
+	if (ht->array[i] == NULL)
+		ht->array[i] = item;
+	else
+	{
+		item->next = ht->array[i];
+		ht->array[i] = item;
+	}
+
+	return (1);
 }
