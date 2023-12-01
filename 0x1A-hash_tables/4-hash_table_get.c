b19c0b056cb8de7bec830823e3c7267780da0cb3
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-01 16:36:37 +0200

    task 4 completed efficiently

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,4 +9,17 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *item;
+	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
+
+	if (ht == NULL)
+		return (NULL);
+	item = ht->array[idx];
+	while (item)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
+	}
+	return (NULL);
 }
