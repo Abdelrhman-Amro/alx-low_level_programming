7f58e39a2273a198aa5493c52e76a73f7da6f9de
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-01 16:45:00 +0200

    Ok now done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,8 +10,11 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *item;
-	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
+	unsigned long int idx;
 
+	if (ht == NULL)
+		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[idx];
 	while (item)
 	{
